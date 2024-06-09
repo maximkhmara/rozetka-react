6238f4f213d1b296d23dc9c9d0a88f7f5d860dd9
Max Kh <maxmac@Maxs-MacBook-Air.local>
2024-06-09 12:21:47 +0300

    products

--- src/components/ProductsCard/ProductsCard.js
+++ src/components/ProductsCard/ProductsCard.js
@@ -1,23 +1,28 @@
-import './ProductsCard.css';
+import "./ProductsCard.css";
 
 const ProductsCard = ({ products }) => {
-	return (
-		<div className='card-wrapper'>
-			{products.map((product) => (
-				<div key={product.id} className='product-card'>
-					<img
-						className='product-card-image'
-						src={product.image}
-						alt={product.name}
-					/>
-					<div>{product.category}</div>
-					<div>{product.name}</div>
-					<div>{product.quantity}</div>
-					<div>{product.price}</div>
-				</div>
-			))}
-		</div>
-	);
+  return (
+    <div className="card-wrapper">
+      {products.map((product) => (
+        <div key={product.id} className="product-card">
+          <img
+            className="product-card-image"
+            src={product.image}
+            alt={product.name}
+          />
+          <div className="product-card-name">{product.name}</div>
+          <div className="product-card-middle">
+            <div className="product-card-price">{product.price}</div>
+            <div className="product-card-quantity">
+              Кількість:
+              {product.quantity}
+            </div>
+          </div>
+          <div className="product-card-status">{product.status}</div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ProductsCard;
--- src/pages/ProductsPage/ProductsPage.js
+++ src/pages/ProductsPage/ProductsPage.js
@@ -8,49 +8,55 @@ const ProductsPage = () => {
 			id: 0,
 			image: 'https://content1.rozetka.com.ua/goods/images/big/47592568.jpg',
 			category: 'PC',
-			name: 'Lenovo Y50-70',
+			name: 'Ноутбук Lenovo Y50-70 Aluminum Black',
 			quantity: 5,
 			price: '25,000.00',
+			status: '🛒 Готовий до відправлення'
 		},
 		{
 			id: 1,
 			image: 'https://content1.rozetka.com.ua/goods/images/big/47592568.jpg',
 			category: 'Clothes',
-			name: 'Nike M Nk Df Acd21',
+			name: 'Ноутбук Lenovo Y50-70 Aluminum Black',
 			quantity: 22,
 			price: '4,000.00',
+			status: '🛒 Готовий до відправлення'
 		},
 		{
 			id: 2,
 			image: 'https://content1.rozetka.com.ua/goods/images/big/47592568.jpg',
 			category: 'Plumbing',
-			name: 'CERSANIT MITO 17',
+			name: 'Ноутбук Lenovo Y50-70 Aluminum Black',
 			quantity: 1337,
 			price: '5,000.00',
+			status: '🛒 Готовий до відправлення'
 		},
 		{
 			id: 3,
 			image: 'https://content1.rozetka.com.ua/goods/images/big/47592568.jpg',
 			category: 'Plumbing',
-			name: 'CERSANIT MITO 17',
+			name: 'Ноутбук Lenovo Y50-70 Aluminum Black',
 			quantity: 1337,
 			price: '5,000.00',
+			status: '🛒 Готовий до відправлення'
 		},
 		{
 			id: 4,
 			image: 'https://content1.rozetka.com.ua/goods/images/big/47592568.jpg',
 			category: 'Plumbing',
-			name: 'CERSANIT MITO 17',
+			name: 'Ноутбук Lenovo Y50-70 Aluminum Black',
 			quantity: 1337,
 			price: '5,000.00',
+			status: '🛒 Готовий до відправлення'
 		},
 		{
 			id: 5,
 			image: 'https://content1.rozetka.com.ua/goods/images/big/47592568.jpg',
 			category: 'Plumbing',
-			name: 'CERSANIT MITO 17',
+			name: 'Ноутбук Lenovo Y50-70 Aluminum Black',
 			quantity: 1337,
 			price: '5,000.00',
+			status: '🛒 Готовий до відправлення'
 		},
 	];
 
