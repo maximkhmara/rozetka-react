9c89df0c9c50111f3c684a14418241170bc00db8
Max Kh <maxmac@Maxs-MacBook-Air.local>
2024-06-10 17:51:21 +0300

    fix

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,29 +1,28 @@
-import './Input.css';
-import { IoEye, IoEyeOff } from 'react-icons/io5'; // додано IoEyeOff для перекресленого ока
-import { useState } from 'react';
+import "./Input.css";
+import { IoEye, IoEyeOff } from "react-icons/io5"; // додано IoEyeOff для перекресленого ока
+import { useState } from "react";
 
 const Input = ({ ourClass, placeholderName, showIcon, initialType }) => {
-	const [isPassword, setIsPassword] = useState(initialType === 'password');
+  const [isPassword, setIsPassword] = useState(initialType === "password");
 
-	const handleClick = () => {
-		setIsPassword((prevIsPassword) => !prevIsPassword);
-	};
+  const handleClick = () => {
+    setIsPassword((prevIsPassword) => !prevIsPassword);
+  };
 
-	return (
-		<label className={ourClass ? `login-input ${ourClass}` : 'login-input'}>
-			<input
-				type={isPassword ? 'password' : 'text'}
-				placeholder={placeholderName}
-			/>
-			{showIcon ? (
-				isPassword ? (
-					<IoEye onClick={handleClick} className='password-eye-icon' />
-				) : (
-					<IoEyeOff onClick={handleClick} className='password-eye-icon' />
-				)
-			) : null}
-		</label>
-	);
+  return (
+    <label className={ourClass ? `login-input ${ourClass}` : "login-input"}>
+      <input
+        type={isPassword ? "password" : "text"}
+        placeholder={placeholderName}
+      />
+      {showIcon &&
+        (isPassword ? (
+          <IoEye onClick={handleClick} className="password-eye-icon" />
+        ) : (
+          <IoEyeOff onClick={handleClick} className="password-eye-icon" />
+        ))}
+    </label>
+  );
 };
 
 export default Input;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-// import LoginPage from './pages/LoginPage/LoginPage';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import LoginPage from "./pages/LoginPage/LoginPage";
 // import ProductsEditPage from './pages/ProductsEditPage/ProductsEditPage';
-import ProductsPage from './pages/ProductsPage/ProductsPage';
+import ProductsPage from "./pages/ProductsPage/ProductsPage";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		{/* <LoginPage /> */}
-		{/* <ProductsEditPage /> */}
-		<ProductsPage />
-	</React.StrictMode>
+  <React.StrictMode>
+    {/* <LoginPage /> */}
+    {/* <ProductsEditPage /> */}
+    <ProductsPage />
+  </React.StrictMode>
 );
