f6abf1777688e1c8178a21b56ebdc94d702f17c2
Maxim Khmara <maxmh100@gmail.com>
2024-06-16 12:57:39 +0300

    hw-28

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import LoginPage from "./pages/LoginPage/LoginPage";
-// import ProductsEditPage from './pages/ProductsEditPage/ProductsEditPage';
+// import LoginPage from "./pages/LoginPage/LoginPage";
+import ProductsEditPage from "./pages/ProductsEditPage/ProductsEditPage";
 // import ProductsPage from "./pages/ProductsPage/ProductsPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <LoginPage />
-    {/* <ProductsEditPage /> */}
+    {/* <LoginPage /> */}
+    <ProductsEditPage />
     {/* <ProductsPage /> */}
   </React.StrictMode>
 );
--- src/pages/ProductsEditPage/ProductsEditPage.js
+++ src/pages/ProductsEditPage/ProductsEditPage.js
@@ -1,48 +1,43 @@
-import './ProductsEditPage.css';
-import IconButton from '../../components/IconButton/IconButton';
-import { FaRegUser } from 'react-icons/fa6';
-import { FiPlus } from 'react-icons/fi';
-import logo2 from '../../assets/images/logo2.svg';
-import ProductTable from '../../components/ProductTable/ProductTable';
+import React, { useState, useEffect } from "react";
+import "./ProductsEditPage.css";
+import IconButton from "../../components/IconButton/IconButton";
+import { FaRegUser } from "react-icons/fa6";
+import { FiPlus } from "react-icons/fi";
+import logo2 from "../../assets/images/logo2.svg";
+import ProductTable from "../../components/ProductTable/ProductTable";
 
 const ProductsEditPage = () => {
-	const products = [
-		{
-			id: 0,
-			category: 'PC',
-			name: 'Lenovo Y50-70',
-			quantity: 5,
-			price: '25,000.00',
-		},
-		{
-			id: 1,
-			category: 'Clothes',
-			name: 'Nike M Nk Df Acd21',
-			quantity: 22,
-			price: '4,000.00',
-		},
-		{
-			id: 2,
-			category: 'Plumbing',
-			name: 'CERSANIT MITO 17',
-			quantity: 1337,
-			price: '5,000.00',
-		},
-	];
+  const [products, setProducts] = useState([]);
 
-	return (
-		<div className='productsEditPage-wrapper'>
-			<div className='productsEditPage-top'>
-				<img src={logo2} alt='Logo' />
-			</div>
-			<div className='productsEditPage-btns'>
-				<IconButton CustomIcon={<FaRegUser />} name='Preview' />
-				<IconButton CustomIcon={<FiPlus />} name='Add' />
-			</div>
-			<div className='productsEditPage-title'>Products</div>
-			<ProductTable products={products} />
-		</div>
-	);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch(
+          "https://666eb129f1e1da2be520e627.mockapi.io/api/v1/products"
+        );
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <div className="productsEditPage-wrapper">
+      <div className="productsEditPage-top">
+        <img src={logo2} alt="Logo" />
+      </div>
+      <div className="productsEditPage-btns">
+        <IconButton CustomIcon={<FaRegUser />} name="Preview" />
+        <IconButton CustomIcon={<FiPlus />} name="Add" />
+      </div>
+      <div className="productsEditPage-title">Products</div>
+      <ProductTable products={products} />
+    </div>
+  );
 };
 
 export default ProductsEditPage;
