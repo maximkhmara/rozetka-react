541f7de86f6d3561a9768b196c2555a1309f8a4b
Maxim Khmara <maxmh100@gmail.com>
2024-07-08 09:11:44 +0300

    w

--- src/components/ModalForm/ModalForm.js
+++ src/components/ModalForm/ModalForm.js
@@ -5,9 +5,15 @@ import Modal from "@mui/material/Modal";
 import "./ModalForm.css";
 import { RxCross1 } from "react-icons/rx";
 import { useForm } from "react-hook-form";
+import { BASE_URL } from "../../apiConfig";
 
-const ModalForm = ({ open, onClose, product, isEditing }) => {
-  const { register, handleSubmit, reset } = useForm();
+const ModalForm = ({ open, onClose, product, isEditing, onProductAdded }) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
 
   useEffect(() => {
     if (isEditing && product) {
@@ -29,9 +35,33 @@ const ModalForm = ({ open, onClose, product, isEditing }) => {
     }
   }, [isEditing, product, reset]);
 
-  const onSubmit = (data) => {
-    console.log(data);
-    onClose();
+  const onSubmit = async (data) => {
+    try {
+      const response = isEditing
+        ? await fetch(`${BASE_URL}/${product.id}`, {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+          })
+        : await fetch(BASE_URL, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+          });
+
+      if (response.ok) {
+        onClose();
+        onProductAdded();
+      } else {
+        console.error("Error saving product", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error saving product", error);
+    }
   };
 
   return (
@@ -46,39 +76,65 @@ const ModalForm = ({ open, onClose, product, isEditing }) => {
           </div>
           <div className="modal-edit-container">
             <TextField
-              className="modal-edit-form-field"
+              className={`modal-edit-form-field ${
+                errors.category ? "error-border" : ""
+              }`}
               id="category"
               name="category"
               label="Category"
               variant="filled"
               {...register("category", { required: "This field is required" })}
+              error={!!errors.category}
             />
+            {errors.category && (
+              <div className="error-message">This field is required</div>
+            )}
             <TextField
-              className="modal-edit-form-field"
+              className={`modal-edit-form-field ${
+                errors.name ? "error-border" : ""
+              }`}
               id="name"
               name="name"
               label="Name"
               variant="filled"
               {...register("name", { required: "This field is required" })}
+              error={!!errors.name}
             />
+            {errors.name && (
+              <div className="error-message">This field is required</div>
+            )}
             <TextField
-              className="modal-edit-form-field"
+              className={`modal-edit-form-field ${
+                errors.quantity ? "error-border" : ""
+              }`}
               id="quantity"
               name="quantity"
               label="Quantity"
               variant="filled"
               {...register("quantity", { required: "This field is required" })}
+              error={!!errors.quantity}
             />
+            {errors.quantity && (
+              <div className="error-message">This field is required</div>
+            )}
             <TextField
-              className="modal-edit-form-field"
+              className={`modal-edit-form-field ${
+                errors.price ? "error-border" : ""
+              }`}
               id="price"
               name="price"
               label="Price"
               variant="filled"
               {...register("price", { required: "This field is required" })}
+              error={!!errors.price}
             />
+            {errors.price && (
+              <div className="error-message">This field is required</div>
+            )}
             <TextField
-              className="modal-edit-form-field"
+              className={`modal-edit-form-field ${
+                errors.description ? "error-border" : ""
+              }`}
               id="description"
               name="description"
               label="Description"
@@ -86,7 +142,11 @@ const ModalForm = ({ open, onClose, product, isEditing }) => {
               {...register("description", {
                 required: "This field is required",
               })}
+              error={!!errors.description}
             />
+            {errors.description && (
+              <div className="error-message">This field is required</div>
+            )}
           </div>
           <div className="modal-edit-btns">
             <button
